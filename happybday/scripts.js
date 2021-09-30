0859e1657e55e4d536337f2e40998d02c5c105da
Lisha McMurtrey <89047845+lishadanae@users.noreply.github.com>
2021-09-30 17:45:27 -0500

    Add files via upload

---
+++ happybday/scripts.js
@@ -0,0 +1,24 @@
+//clicking "new" gets a new dog gif
+$(document).ready(function(){
+    $("#new").click(function() {
+      var imgSrc;
+      var imgId;
+      $.ajax ({
+        url: "//api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=dog",
+        type: "GET",
+        dataType: "json",
+        success: function(response) {
+          imgSrc = response.data.image_url;
+          imgId = response.data.id;
+          console.log(response);
+          $("#giphy-embed").attr("src", "https://giphy.com/embed/"+imgSrc);
+          $("#tweetButton").attr("href", "https://giphy.com/gifs/"+imgId+"/tweet")
+          return false;
+        }, 
+        error: function(e) {
+          console.log("uh oh");
+          }
+        });
+    });
+  });
+  
